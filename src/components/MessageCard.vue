b21468912b287ee3cd4e03991766f803ac7ad6ea
Luc Mergault <contact@luc-mergault.fr>
2025-03-23 00:05:08 +0100

    Parse markdown

--- src/components/MessageCard.vue
+++ src/components/MessageCard.vue
@@ -1,6 +1,8 @@
 
 <script setup>
 
+import { marked } from 'marked';
+
 const props = defineProps({
     message: {
         type: Object,
@@ -11,14 +13,17 @@ const props = defineProps({
         default: "left"
     },
 });
+
+const parsedMessage = marked.parse(props.message.content);
+
 </script>
 
 <template>
   <div class="chat-bubble bg-gradient-to-r from-indigo-50 to-cyan-50 text-gray-700 shadow-sm relative overflow-hidden" v-if="direction == 'left'">
     <div class="absolute -bottom-6 -left-6 w-12 h-12 bg-indigo-200 rounded-full opacity-20"></div>
-    {{ message.content }}
+    <div v-html="parsedMessage"></div>
   </div>
   <div class="chat-bubble bg-gradient-to-br from-amber-400 to-orange-400 text-white shadow-sm relative overflow-hidden" v-else>
-    {{ message.content }}
+    <div v-html="parsedMessage"></div>
   </div>
 </template>
